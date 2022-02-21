15bb78cd34fa3c355d271efcf0f87da38586070f
Ruby Adkins <radkins22@students.stab.org>
2022-02-21 09:12:51 -0500

    refresh page when user returns home

---
+++ public/client.js
@@ -0,0 +1,11 @@
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  });
+
+  console.log("hello world")
