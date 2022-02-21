d167ca132d03f15f18068a34dd0c418bb1f0b2af
Ruby Adkins <radkins22@students.stab.org>
2022-02-20 21:36:54 -0500

    turn leds on/pff

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 const http = require('http').Server(app)
 const bodyParser = require('body-parser')
 const engines = require('consolidate')
+const { Console } = require('console')
 
 //setup app
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -19,13 +20,35 @@ app.use(express.static('public'))
 app.engine('html', engines.hogan)
 app.set('views', __dirname + '/views')
 
+//led pin location lists
+let pinlocs = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3]
+
 app.get('/', (req, res) => {
+    //turn all leds off
+    for (let i = 0; i < pinlocs.length; i++) {
+      var pin = new Led(pinlocs[i])
+      pin.off()
+    }
   res.render('index.html', {})
 })
 
 app.post('/turnledon', (req, res) => {
-  var led = new Led(parseInt(req.body.submit))
-  led.on()
+  //turn other leds off
+  for (let i = 0; i < pinlocs.length; i++) {
+    var pin = new Led(pinlocs[i])
+    pin.off()
+  }
+
+  //check if need to light both brit and france
+  if (req.body.submit == "11&10") {
+    var led1 = new Led(11)
+    var led2 = new Led(10)
+    led1.on()
+    led2.on()
+  } else {
+    var led = new Led(parseInt(req.body.submit))
+    led.on()
+  }
   res.render('index.html', {})
 })
 
